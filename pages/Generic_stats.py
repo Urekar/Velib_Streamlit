83bb16d83e8cdb1d73e3422ec692cae147677125
Urekar <nicolas.bouttier@gmail.com>
2026-02-05 22:26:22 +0100

    secret stream

--- pages/Generic_stats.py
+++ pages/Generic_stats.py
@@ -24,7 +24,9 @@ st.caption(
 # ====================================================
 key_path = "/home/nicolas/Streamlit-app/gcp_sa_key/streamlit-to-gcp-sa.json"
 
-credentials = service_account.Credentials.from_service_account_file(key_path)
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
 client = bigquery.Client(
     credentials=credentials,
     project=credentials.project_id
